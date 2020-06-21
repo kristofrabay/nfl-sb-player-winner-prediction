35ad0ef24d2ed6da5cb9ea4cf3c32cf2c27eb931
kristofrabay <kristof.rabay@gmail.com>
2020-06-21 17:03:59 +0200

    build sb winner predictors too

--- analysis/modeling.R
+++ analysis/modeling.R
@@ -17,7 +17,10 @@ naniar::vis_miss(data)
 data <- data.frame(data)
 data <- data[, which(colMeans(!is.na(data)) > 0.5)]
 data <- data.table(data)
-# sb player prediction
+
+#############################################
+########### sb player prediction ############
+#############################################
 
 data[, win_SB := NULL]
 data[, season := NULL]
@@ -138,7 +141,7 @@ XGB$results[XGB$results$max_depth == XGB$bestTune$max_depth &
 pred <- predict(XGB, newdata = test, type = 'prob')$played_sb
 act <- ifelse(test$play_SB == "played_sb", 1, 0)
 roc_obj <- roc(act, pred)
-auc(roc_obj) # 0.9 AUC
+auc(roc_obj)
 
 plot(roc_obj, 
      print.thres = "best", auc.polygon = T, print.auc = T,
@@ -188,7 +191,7 @@ melt(varImps) %>%
   tidytext::scale_x_reordered() +
   facet_wrap(~ variable, scales = 'free')
 
-ggsave("plots/feature_importances_non_scaled.png", device = 'png', plot = last_plot())
+ggsave("plots/feature_importances_non_scaled_play_SB.png", device = 'png', plot = last_plot())
 
 
 ### enhance dataset with predictions
@@ -206,3 +209,140 @@ write_csv(preds_making_it_to_sb_gbm, "data/predictions_to_make_it_to_sb.csv")
   
 View(preds_making_it_to_sb_gbm)
 
+
+
+
+#############################################
+########### sb winner prediction ############
+#############################################
+
+
+# these are just default models
+# TODO fine tune win-predictors
+
+
+data <- readRDS("data/labeled_stats.RDS")
+data_as_is <- readRDS("data/labeled_stats.RDS")
+
+data <- data.frame(data)
+data <- data[, which(colMeans(!is.na(data)) > 0.5)]
+data <- data.table(data)
+
+data[, play_SB := NULL]
+data[, season := NULL]
+
+data$win_SB <- ifelse(data$win_SB == 1, 'won_sb', 'lost/missed_sb')
+data$win_SB <- factor(data$win_SB)
+
+set.seed(20202020)
+train_index <- createDataPartition(data[["win_SB"]], p = 0.75, list = F)
+train <- data[train_index,]
+test <- data[-train_index]
+
+
+
+trC <- trainControl(method = "CV", number = 3)
+
+
+set.seed(20202020)
+
+GBM_w <- train(win_SB ~ . -Team,
+             method = "gbm",
+             data = train,
+             trControl = trC,
+             tuneGrid = expand.grid(n.trees = 500, 
+                                    interaction.depth = c(7), # 7, 8, 9 
+                                    shrinkage = c(0.001), # 0.01, 0.005, 0.001
+                                    n.minobsinnode = c(14)), #14, 15 ,16
+             verbose = T)
+
+GBM_w$results
+
+GBM_w$results[GBM_w$results$interaction.depth == GBM_w$bestTune$interaction.depth & 
+                GBM_w$results$n.minobsinnode == GBM_w$bestTune$n.minobsinnode &
+                GBM_w$results$shrinkage == GBM_w$bestTune$shrinkage,]
+
+
+pred <- predict(GBM_w, newdata = test, type = 'prob')$won_sb
+act <- ifelse(test$win_SB == "won_sb", 1, 0)
+roc_obj <- roc(act, pred)
+auc(roc_obj)
+
+
+set.seed(20202020)
+
+XGB_w <- train(win_SB ~ . -Team,
+             method = "xgbTree",
+             data = train,
+             trControl = trC,
+             tuneGrid = expand.grid(nrounds = 500,
+                                    max_depth = c(1), # 1, 2
+                                    eta = c(0.005),
+                                    gamma = 0.1,
+                                    colsample_bytree = c(1/3), # 1/3, 0.4
+                                    min_child_weight = c(7), # 7, 10, 13
+                                    subsample = c(0.5))) # 0.5, 2/3
+
+XGB_w$results %>% arrange(desc(Accuracy))
+
+XGB_w$results[XGB_w$results$max_depth == XGB_w$bestTune$max_depth & 
+                XGB_w$results$eta == XGB_w$bestTune$eta &
+                XGB_w$results$colsample_bytree == XGB_w$bestTune$colsample_bytree &
+                XGB_w$results$min_child_weight == XGB_w$bestTune$min_child_weight &
+                XGB_w$results$subsample == XGB_w$bestTune$subsample,]
+
+
+pred <- predict(XGB_w, newdata = test, type = 'prob')$won_sb
+act <- ifelse(test$win_SB == "won_sb", 1, 0)
+roc_obj <- roc(act, pred)
+auc(roc_obj)
+
+
+varimpGBM_w <- varImp(GBM_w)$importance
+varimpGBM_w$feature <- rownames(varimpGBM_w)
+
+varimpXGB_w <- varImp(XGB_w)$importance
+varimpXGB_w$feature <- rownames(varimpXGB_w)
+
+varImps_w <- merge(x = varimpGBM_w, y = varimpXGB_w, by = "feature")
+colnames(varImps_w) <- c("feature", "GBM", "XGB")
+
+varImps_w %>% arrange(desc(GBM), desc(XGB))
+
+
+melt(varImps_w) %>% 
+  group_by(variable) %>% 
+  top_n(15, value) %>% 
+  ungroup() %>% 
+  mutate(variable = as.factor(variable),
+         feature = as.factor(feature),
+         feature = tidytext::reorder_within(feature, value, variable)) %>%
+  ggplot(aes(feature, value, color = variable, fill = variable)) +
+  geom_col(position = 'dodge', show.legend = F) +
+  coord_flip() +
+  labs(subtitle = 'Most important stats deciding who wins the Super Bowl',
+       title = 'Allowed passed TDs by defense, passed yards / attemps by offense are most deciding stats',
+       caption = 'Non-scaled features',
+       x = 'Statistic',
+       y = 'Importance') +
+  theme_bw() +
+  tidytext::scale_x_reordered() +
+  facet_wrap(~ variable, scales = 'free')
+
+ggsave("plots/feature_importances_non_scaled_win_SB.png", device = 'png', plot = last_plot())
+
+
+### enhance dataset with predictions
+data_w_predictions_w <- data.table()
+data_w_predictions_w[, team := data_as_is$Team]
+data_w_predictions_w[, season := data_as_is$season]
+data_w_predictions_w[, season := factor(season)]
+data_w_predictions_w[, won_sb := data_as_is$win_SB]
+data_w_predictions_w[, prediction_GBM := predict(GBM_w, data, type = 'prob')$won_sb]
+#data_w_predictions[, prediction_XGB := predict(XGB, data, type = 'prob')$played_sb]
+
+preds_winning_sb_gbm <- data_w_predictions_w %>% arrange(desc(prediction_GBM))
+
+write_csv(preds_winning_sb_gbm, "data/predictions_to_win_the_sb.csv")
+
+View(preds_winning_sb_gbm)
