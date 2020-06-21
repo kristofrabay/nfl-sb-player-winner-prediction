bb22d3716dd150270bc2abc75eeab5c2364abbcd
kristofrabay <kristof.rabay@gmail.com>
2020-06-21 16:46:07 +0200

    add xgb, varimps, gbm performed better

--- analysis/modeling.R
+++ analysis/modeling.R
@@ -1,12 +1,15 @@
 library(data.table)
 library(caret)
+library(dataPreparation)
 library(xgboost)
 library(pROC)
 library(rpart)
 library(rpart.plot)
+library(gbm)
 library(tidyverse)
 
 data <- readRDS("data/labeled_stats.RDS")
+data_as_is <- readRDS("data/labeled_stats.RDS")
 
 # let's actually drop NAs (defensive scoring stats since 2009)
 
@@ -29,13 +32,21 @@ train_index <- createDataPartition(data[["play_SB"]], p = 0.75, list = F)
 train <- data[train_index,]
 test <- data[-train_index]
 
+# scale features
+
+#numerics <- names(select_if(train, is.numeric))
+
+#scales <- build_scales(dataSet = train, cols = numerics, verbose = F)
+#fastScale(dataSet = train, scales = scales, verbose = F)
+#fastScale(dataSet = test, scales = scales, verbose = F) 
+
 
 # control
 
 trC <- trainControl(method = "CV", number = 3)
 
 
-# simple cart
+### simple cart
 
 set.seed(20202020)
 
@@ -47,7 +58,7 @@ CART <- train(play_SB ~ . -Team,
 
 rpart.plot(CART$finalModel,
            type = 1, 
-           extra = 101,
+           extra = 4,
            leaf.round = 0,
            fallen.leaves = F, 
            branch = 1, 
@@ -59,7 +70,7 @@ rpart.plot(CART$finalModel,
 # 1. offensive scored points > 426 AND defensive allowed rush TDs < 8
 # 1. offensive scored points > 426 AND defensive allowed rush TDs >= 8 AND defense allowed pass rating < 72
 
-# gbm
+### gbm
 
 set.seed(20202020)
 
@@ -68,9 +79,9 @@ GBM <- train(play_SB ~ . -Team,
              data = train,
              trControl = trC,
              tuneGrid = expand.grid(n.trees = 500, 
-                                    interaction.depth = c(7, 8, 9), 
-                                    shrinkage = c(0.01, 0.005, 0.001),
-                                    n.minobsinnode = c(14, 15, 16)),
+                                    interaction.depth = c(7), # 7, 8, 9 
+                                    shrinkage = c(0.001), # 0.01, 0.005, 0.001
+                                    n.minobsinnode = c(14)), #14, 15 ,16
              verbose = T)
 
 GBM$results
@@ -93,28 +104,105 @@ plot(roc_obj,
 
 
 probs <- predict(GBM, test, type = 'prob')
+threshold <- 0.1
+pred      <- factor(ifelse(probs[, "played_sb"] > threshold, "played_sb", "did_not_play_sb") )
+confusionMatrix(pred, test$play_SB)
+
+
+
+### xgboost
+
+set.seed(20202020)
+
+XGB <- train(play_SB ~ . -Team,
+             method = "xgbTree",
+             data = train,
+             trControl = trC,
+             tuneGrid = expand.grid(nrounds = 500,
+                                    max_depth = c(1), # 1, 2
+                                    eta = c(0.005),
+                                    gamma = 0.1,
+                                    colsample_bytree = c(1/3), # 1/3, 0.4
+                                    min_child_weight = c(7), # 7, 10, 13
+                                    subsample = c(0.5))) # 0.5, 2/3
+
+XGB$results %>% arrange(desc(Accuracy))
+
+XGB$results[XGB$results$max_depth == XGB$bestTune$max_depth & 
+              XGB$results$eta == XGB$bestTune$eta &
+              XGB$results$colsample_bytree == XGB$bestTune$colsample_bytree &
+              XGB$results$min_child_weight == XGB$bestTune$min_child_weight &
+              XGB$results$subsample == XGB$bestTune$subsample,]
+
+
+pred <- predict(XGB, newdata = test, type = 'prob')$played_sb
+act <- ifelse(test$play_SB == "played_sb", 1, 0)
+roc_obj <- roc(act, pred)
+auc(roc_obj) # 0.9 AUC
+
+plot(roc_obj, 
+     print.thres = "best", auc.polygon = T, print.auc = T,
+     print.auc.x = 0.25, print.auc.y = 0.25,
+     xlab = "1 - False Positive Rate", ylab = "True Positive Rate",
+     grid=c(0.1, 0.2), max.auc.polygon = T, auc.polygon.col = "lightblue")
+
+
+probs <- predict(XGB, test, type = 'prob')
 threshold <- 0.2
 pred      <- factor(ifelse(probs[, "played_sb"] > threshold, "played_sb", "did_not_play_sb") )
 confusionMatrix(pred, test$play_SB)
 
 
-# xgboost
 
-# TODO
+### variable imporatances
 
 
-# enhance dataset with predictions
+varimpGBM <- varImp(GBM)$importance
+varimpGBM$feature <- rownames(varimpGBM)
+
+varimpXGB <- varImp(XGB)$importance
+varimpXGB$feature <- rownames(varimpXGB)
+
+varImps <- merge(x = varimpGBM, y = varimpXGB, by = "feature")
+colnames(varImps) <- c("feature", "GBM", "XGB")
+
+varImps %>% arrange(desc(GBM), desc(XGB))
+
+
+melt(varImps) %>% 
+  group_by(variable) %>% 
+  top_n(15, value) %>% 
+  ungroup() %>% 
+  mutate(variable = as.factor(variable),
+         feature = as.factor(feature),
+         feature = tidytext::reorder_within(feature, value, variable)) %>%
+  ggplot(aes(feature, value, color = variable, fill = variable)) +
+  geom_col(position = 'dodge', show.legend = F) +
+  coord_flip() +
+  labs(subtitle = 'Most important stats deciding who makes it to the Super Bowl',
+       title = 'Scored points by offense, QB pass rating, # of PAT attempts & allowed rush TDs are most deciding stats',
+       caption = 'Non-scaled features',
+       x = 'Statistic',
+       y = 'Importance') +
+  theme_bw() +
+  tidytext::scale_x_reordered() +
+  facet_wrap(~ variable, scales = 'free')
+
+ggsave("plots/feature_importances_non_scaled.png", device = 'png', plot = last_plot())
+
+
+### enhance dataset with predictions
 data_w_predictions <- data.table()
-data_w_predictions[, team := data$Team]
-data_w_predictions[, season := data$season]
+data_w_predictions[, team := data_as_is$Team]
+data_w_predictions[, season := data_as_is$season]
 data_w_predictions[, season := factor(season)]
-data_w_predictions[, played_sb := data$play_SB]
-data_w_predictions[, prediction := predict(GBM, data, type = 'prob')$played_sb]
-str(data_w_predictions)
+data_w_predictions[, played_sb := data_as_is$play_SB]
+data_w_predictions[, prediction_GBM := predict(GBM, data, type = 'prob')$played_sb]
+#data_w_predictions[, prediction_XGB := predict(XGB, data, type = 'prob')$played_sb]
 
-preds_making_it_to_sb_gbm <- data_w_predictions %>% 
-  arrange(desc(prediction))
+preds_making_it_to_sb_gbm <- data_w_predictions %>% arrange(desc(prediction_GBM))
 
 write_csv(preds_making_it_to_sb_gbm, "data/predictions_to_make_it_to_sb.csv")
   
+View(preds_making_it_to_sb_gbm)
 
