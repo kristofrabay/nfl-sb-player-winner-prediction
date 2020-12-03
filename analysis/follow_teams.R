e5408bf0c562c5e61262b38cf60adaf84bde49ec
kristofrabay <kristof.rabay@gmail.com>
2020-12-03 21:57:19 +0100

    2020 dec 3 scrape run

--- analysis/follow_teams.R
+++ analysis/follow_teams.R
@@ -19,34 +19,57 @@ winning <- Reduce(function(x, y) merge(x, y, all=TRUE), winning)
 playing$mean <- playing %>% select_if(is.numeric) %>% rowMeans()
 winning$mean <- winning %>% select_if(is.numeric) %>% rowMeans()
 
+View(playing)
+View(winning)
+
 # order by mean, select top 5
 
-playing_top <- playing %>% arrange(desc(mean)) %>% top_n(5)
-winning_top <- winning %>% arrange(desc(mean)) %>% top_n(5)
+#playing_top <- playing %>% arrange(desc(mean)) %>% top_n(5)
+#winning_top <- winning %>% arrange(desc(mean)) %>% top_n(5)
 
-playing_top$mean <- NULL
-winning_top$mean <- NULL
+playing <- playing %>% filter(team %in% c('Seahawks', 'Chiefs', 'Saints', 'Packers', 'Cardinals', 'Steelers', 
+                               'Buccanneers', 'Rams', 'Titans', 'Colts', 'Ravens', 'Bills'))
 
-playing_top <- playing_top %>% gather(date, predictions, '2020-11-03' : names(playing_top)[length(names(playing_top))])
-winning_top <- winning_top %>% gather(date, predictions, '2020-11-03' : names(winning_top)[length(names(winning_top))])
+winning <- winning %>% filter(team %in% c('Seahawks', 'Chiefs', 'Saints', 'Packers', 'Cardinals', 'Steelers', 
+                               'Buccanneers', 'Rams', 'Titans', 'Colts', 'Ravens', 'Bills'))
 
-playing_top$date <- as.Date(playing_top$date, "%Y-%m-%d")
-winning_top$date <- as.Date(winning_top$date, "%Y-%m-%d")
+
+playing$mean <- NULL
+winning$mean <- NULL
+
+playing <- playing %>% gather(date, predictions, '2020-11-03' : names(playing)[length(names(playing))])
+winning <- winning %>% gather(date, predictions, '2020-11-03' : names(winning)[length(names(winning))])
+
+playing$date <- as.Date(playing$date, "%Y-%m-%d")
+winning$date <- as.Date(winning$date, "%Y-%m-%d")
 
 # plot
 
-playing_top %>% ggplot(aes(date, predictions, fill = team, color = team, shape = team)) +
-  geom_line(size = 0.75) + 
-  geom_point(size = 2.5) + 
+playing %>% filter(team %in% c('Seahawks', 'Chiefs', 'Saints', 'Packers', 'Cardinals', 'Steelers')) %>% 
+  ggplot(aes(date, predictions, fill = team, color = team, shape = team)) +
+  geom_line(size = 2/3) + 
+  geom_point(size = 2) + 
   labs(title = 'Teams with largest average SB probabilities since week 8',
        subtitle = 'Making it to the big game', 
        x = 'Date', y = 'Predicted (unnormalized) probability') +
   theme_bw() +
   theme(legend.position = "bottom")
-ggsave('../plots/AAA_predictions_2020_making_it.png')
+ggsave('../plots/AAA_predictions_2020_making_itA.png')
+
+playing %>% filter(!team %in% c('Seahawks', 'Chiefs', 'Saints', 'Packers', 'Cardinals', 'Steelers')) %>% 
+  ggplot(aes(date, predictions, fill = team, color = team, shape = team)) +
+  geom_line(size = 2/3) + 
+  geom_point(size = 2) + 
+  labs(title = 'Teams with largest average SB probabilities since week 8',
+       subtitle = 'Making it to the big game', 
+       x = 'Date', y = 'Predicted (unnormalized) probability') +
+  theme_bw() +
+  theme(legend.position = "bottom")
+ggsave('../plots/AAA_predictions_2020_making_itB.png')
 
 
-winning_top %>% ggplot(aes(date, predictions, fill = team, color = team, shape = team)) +
+winning %>% filter(team %in% c('Seahawks', 'Chiefs', 'Saints', 'Packers', 'Cardinals', 'Steelers')) %>% 
+  ggplot(aes(date, predictions, fill = team, color = team, shape = team)) +
   geom_line(size = 2/3) + 
   geom_point(size = 2) + 
   labs(title = 'Teams with largest average SB probabilities since week 8',
@@ -54,5 +77,16 @@ winning_top %>% ggplot(aes(date, predictions, fill = team, color = team, shape =
        x = 'Date', y = 'Predicted (unnormalized) probability') +
   theme_bw() +
   theme(legend.position = "bottom")
-ggsave('../plots/AAA_predictions_2020_winning_it.png')
+ggsave('../plots/AAA_predictions_2020_winning_itA.png')
 
+
+winning %>% filter(!team %in% c('Seahawks', 'Chiefs', 'Saints', 'Packers', 'Cardinals', 'Steelers')) %>% 
+  ggplot(aes(date, predictions, fill = team, color = team, shape = team)) +
+  geom_line(size = 2/3) + 
+  geom_point(size = 2) + 
+  labs(title = 'Teams with largest average SB probabilities since week 8',
+       subtitle = 'Winning the big game', 
+       x = 'Date', y = 'Predicted (unnormalized) probability') +
+  theme_bw() +
+  theme(legend.position = "bottom")
+ggsave('../plots/AAA_predictions_2020_winning_itB.png')
